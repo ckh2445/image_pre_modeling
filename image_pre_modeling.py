1917ae04d553fb45776b5a72d0bbdf92cbffdd40
pyyolo <79975403+ckh2445@users.noreply.github.com>
2022-08-15 18:32:36 +0900

    2022-08-15
    2022-08-15

--- image_pre_modeling.py
+++ image_pre_modeling.py
@@ -9,11 +9,20 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.callbacks import ModelCheckpoint
 
+gpus= tf.config.experimental.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(gpus[0], True)
+
 class tensorflow():
     def __init__(self):
         self.url = 'https://storage.googleapis.com/download.tensorflow.org/data/rps.zip'
         self.DIR = "tmp/rps/"
-        self.original_generator = ""
+        self.original_datagen = ImageDataGenerator(rescale=1./255)
+        self.original_generator = self.original_datagen.flow_from_directory(self.DIR, 
+                                                                batch_size=128, 
+                                                                target_size=(150, 150), 
+                                                                class_mode='categorical'
+                                                                )
+        
     def dataset_load(self):
         urllib.request.urlretrieve(self.url, 'rps.zip')
         self.local_zip = 'rps.zip'
@@ -48,19 +57,12 @@ class tensorflow():
                                                           subset='validation', 
                                                          )
         
-    def veiw_image(self):
+    def view_image(self):
         class_map = {
             0: 'Paper',
             1: 'Rock', 
             2: 'Scissors'
         }
-
-        original_datagen = ImageDataGenerator(rescale=1./255)
-        self.original_generator = original_datagen.flow_from_directory(self.DIR, 
-                                                                batch_size=128, 
-                                                                target_size=(150, 150), 
-                                                                class_mode='categorical'
-                                                                )
 
         for x, y in self.original_generator:
             print(x.shape, y.shape)
@@ -90,9 +92,9 @@ class tensorflow():
             plt.show()
             break
         
-    def conv2D(self):
+    def makemodel(self):
         for x, y in self.original_generator:
-            pic = x[:5]
+            self.pic = x[:5]
             break
         
         conv2d = Conv2D(64, (3, 3), input_shape=(150, 150, 3))
@@ -101,46 +103,70 @@ class tensorflow():
         fig, axes = plt.subplots(8, 8)
         fig.set_size_inches(16, 16)
         for i in range(64):
-            axes[i//8, i%8].imshow(conv2d(pic)[0,:,:,i], cmap='gray')
+            axes[i//8, i%8].imshow(conv2d(self.pic)[0,:,:,i], cmap='gray')
             axes[i//8, i%8].axis('off')
             
         fig, axes = plt.subplots(8, 8)
         fig.set_size_inches(16, 16)
         for i in range(64):
-            axes[i//8, i%8].imshow(MaxPooling2D(2, 2)(conv2d(pic))[0, :, :, i], cmap='gray')
+            axes[i//8, i%8].imshow(MaxPooling2D(2, 2)(conv2d(self.pic))[0, :, :, i], cmap='gray')
             axes[i//8, i%8].axis('off')
-        conv1 = Conv2D(64, (3, 3), input_shape=(150, 150, 3))(pic)
+        conv1 = Conv2D(64, (3, 3), input_shape=(150, 150, 3))(self.pic)
         max1 = MaxPooling2D(2, 2)(conv1)
         conv2 = Conv2D(64, (3, 3))(max1)
         max2 = MaxPooling2D(2, 2)(conv2)
         conv3 = Conv2D(64, (3, 3))(max2)
         max3 = MaxPooling2D(2, 2)(conv3)
-        model = Sequential([
-        # Conv2D, MaxPooling2D 조합으로 층을 쌓습니다. 첫번째 입력층의 input_shape은 (150, 150, 3)으로 지정합니다.
-        Conv2D(64, (3, 3), activation='relu', input_shape=(150, 150, 3)),
-        MaxPooling2D(2, 2), 
-        Conv2D(64, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2), 
-        Conv2D(128, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2), 
-        Conv2D(128, (3, 3), activation='relu'),
-        MaxPooling2D(2, 2), 
-        # 2D -> 1D로 변환을 위하여 Flatten 합니다.
-        Flatten(), 
-        # 과적합 방지를 위하여 Dropout을 적용합니다.
-        Dropout(0.5),
-        Dense(512, activation='relu'),
-        # Classification을 위한 Softmax 
-        # 출력층의 갯수는 클래스의 갯수와 동일하게 맞춰줍니다 (3개), activation도 잊지마세요!
-        Dense(3, activation='softmax'),
-        ])
         
+        #model
+        model = Sequential([
+            # Conv2D, MaxPooling2D 조합으로 층을 쌓습니다. 첫번째 입력층의 input_shape은 (150, 150, 3)으로 지정합니다.
+            Conv2D(64, (3, 3), activation='relu', input_shape=(150, 150, 3)),
+            MaxPooling2D(2, 2), 
+            Conv2D(64, (3, 3), activation='relu'),
+            MaxPooling2D(2, 2), 
+            Conv2D(128, (3, 3), activation='relu'),
+            MaxPooling2D(2, 2), 
+            Conv2D(128, (3, 3), activation='relu'),
+            MaxPooling2D(2, 2), 
+            # 2D -> 1D로 변환을 위하여 Flatten 합니다.
+            Flatten(), 
+            # 과적합 방지를 위하여 Dropout을 적용합니다.
+            Dropout(0.5),
+            Dense(512, activation='relu'),
+            # Classification을 위한 Softmax 
+            # 출력층의 갯수는 클래스의 갯수와 동일하게 맞춰줍니다 (3개), activation도 잊지마세요!
+            Dense(3, activation='softmax'),
+        ])
+        model.summary()
+        model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['acc'])
+        checkpoint_path = "tmp_checkpoint.ckpt"
+        checkpoint = ModelCheckpoint(filepath=checkpoint_path, 
+                                    save_weights_only=True, 
+                                    save_best_only=True, 
+                                    monitor='val_loss', 
+                                    verbose=1)
+        epochs=25
+        history = model.fit(self.training_generator, 
+                    validation_data=(self.validation_generator),
+                    epochs=epochs,
+                    callbacks=[checkpoint],
+                    )
+        model.load_weights(checkpoint_path)
+        plt.figure(figsize=(12, 9))
+        plt.plot(np.arange(1, epochs+1), history.history['acc'])
+        plt.plot(np.arange(1, epochs+1), history.history['loss'])
+        plt.title('Acc / Loss', fontsize=20)
+        plt.xlabel('Epochs')
+        plt.ylabel('Acc / Loss')
+        plt.legend(['acc', 'loss'], fontsize=15)
+        plt.show()
 if __name__ == "__main__":
     tensor = tensorflow()
     
-    tensor.dataset_load()
+    #tensor.dataset_load()
     tensor.Image_Generator()
     tensor.Make_Generator()
     
     #tensor.veiw_image()
-    tensor.conv2D()
+    tensor.makemodel()
